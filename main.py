3cf2d2e52a46d5e26036c3e4b8645050ade16289
ranveersinghchouhan13-del <ranveersinghchouhan13@gmail.com>
2026-06-04 21:10:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+print("🚀 Tera Domain Sniper Bot Active Ho Gaya Hai!")
+
+# Yeh loop isko 24 gante zinda rakhega
+while True:
+    print("🔄 Scanning internet data for expired assets...")
+    # Har 10 minute mein scan karega
+    time.sleep(600)
